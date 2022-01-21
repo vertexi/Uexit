bd95b7e4db5f167d472921876a219c1c3fe2425f
Vertexi <778950965@qq.com>
2022-01-21 20:32:44 +0800

    make class implement

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import sys
-from threading import Thread
-from queue import Queue, Empty
 from subprocess import PIPE, Popen
-from proc_parse import collect_proc
+from proc_parse import CollectProcess
 
+sys.path.append(".")
 ON_POSIX = 'posix' in sys.builtin_module_names
-
 
 if __name__ == '__main__':
     process = Popen(["./handle64.exe", "E:\\"], stdout=PIPE, bufsize=1, close_fds=ON_POSIX)
-    processes = []
-    proc_parse_proc = Thread(target=collect_proc, args=(process.stdout, processes))
-    proc_parse_proc.start()
+    collect_proc = CollectProcess(process.stdout)
+
--- proc_parse.py
+++ proc_parse.py
@@ -1,10 +1,43 @@
 import _io
+from PyQt5.QtCore import pyqtSignal, QObject
+from threading import Thread
+
+
+class CollectProcess(QObject):
+    append_signal = pyqtSignal(list)
+
+    def __init__(self, str_reader: _io.BufferedReader):
+        super().__init__()
+        self.processes = []
+
+        # start a thread to parse the buffer string
+        proc_parse_proc = Thread(target=self.collect_proc, args=(str_reader,))
+        proc_parse_proc.start()
+
+    def collect_proc(self, str_reader: _io.BufferedReader):
+        """
+        :param str_reader: buffer string
+        storage parser result list(process_name, pid, file_name) append to self.processes
+        """
+
+        # skip the first useless information
+        for _ in range(5):
+            str_reader.readline()
+
+        # read handle.exe output stream send to proc_parser get process info
+        for line in iter(str_reader.readline, b''):
+            self.processes.append(proc_parser(line))
+
+    def append(self, process):
+        self.processes.append(process)
+        # self.append_signal.emit(process)
+
 
 def proc_parser(process_str: bytes):
     """
     split process_str to process_name pid and file_name
     Example string
-    jcef_helper.exe    pid: 25532  type: File           308: E:\Program Files\percent.pak
+    jcef_helper.exe    pid: 25532  type: File           308: E:\\Program Files\\percent.pak
     :param process_str:
     :return: list(process_name, pid, file_name)
     """
@@ -13,7 +46,7 @@ def proc_parser(process_str: bytes):
     process_str = process_str.split(": ")  # split string
 
     pid_pos = process_str[0].rfind("pid")  # find pid string position
-    process_name = process_str[0][:pid_pos].strip() # get process name
+    process_name = process_str[0][:pid_pos].strip()  # get process name
 
     type_pos = process_str[1].rfind("type")  # find type string position
     pid = process_str[1][:type_pos].strip()  # get pid
@@ -21,11 +54,3 @@ def proc_parser(process_str: bytes):
     file_name = process_str[3].strip()
 
     return list([process_name, pid, file_name])
-
-
-def collect_proc(str_reader: _io.BufferedReader, processes: list):
-    for _ in range(5):
-        str_reader.readline()
-    for line in iter(str_reader.readline, b''):
-        processes.append(proc_parser(line))
-
