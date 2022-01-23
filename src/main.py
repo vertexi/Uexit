da830adb8329f9d78e271fb8c386337bd2e2ad5d
Vertexi <778950965@qq.com>
2022-01-24 01:02:30 +0800

    add icon and app id

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QPixmap, QIcon
 from ui import MainWindow
+import os
 
 sys.path.append(".")
-ON_POSIX = 'posix' in sys.builtin_module_names
+# make a custom application id
+try:
+    # Include in try/except block if you're also targeting Mac/Linux
+    from PyQt5.QtWinExtras import QtWin
+    QtWin.setCurrentProcessExplicitAppUserModelID('com.yhsxz.Uexit.1.0')
+except ImportError:
+    pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    # make icon
+    icon_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'res', 'icon.ico'))
+    app.setWindowIcon(QIcon(QPixmap(icon_file)))
+
     main_window = MainWindow()  # initialize main window
     app.exec_()  # run
